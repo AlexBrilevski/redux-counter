32d2dc961083f80014914a54065ad0d02ad61664
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-08 20:51:41 +0300

    Store was created for the counter app

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from "redux";
+
+const initState = {
+  counter: 0,
+};
+
+const counterReducer = ({ state = initState, action }) => {
+  switch (action.type) {
+    case "INCREMENT": {
+      return { counter: state.counter + 1 };
+    }
+    case "DECREMENT": {
+      return { counter: state.counter - 1 };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+const store = createStore(counterReducer);
+
+export default store;
