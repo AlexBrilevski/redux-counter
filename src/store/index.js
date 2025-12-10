79ac3206b550aafc338fc12951c405777275e24e
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-10 20:13:36 +0300

    The toggle counter action was added

--- src/store/index.js
+++ src/store/index.js
@@ -2,18 +2,22 @@ import { createStore } from "redux";
 
 const initState = {
   counter: 0,
+  showCounter: true,
 };
 
 const counterReducer = (state = initState, action) => {
   switch (action.type) {
     case "INCREMENT": {
-      return { counter: state.counter + 1 };
+      return { ...state, counter: state.counter + 1 };
     }
     case "INCREASE": {
-      return { counter: state.counter + action.payload };
+      return { ...state, counter: state.counter + action.payload };
     }
     case "DECREMENT": {
-      return { counter: state.counter - 1 };
+      return { ...state, counter: state.counter - 1 };
+    }
+    case "TOGGLE": {
+      return { ...state, showCounter: !state.showCounter };
     }
     default: {
       return state;
