8ab22ffe19179089c0094a5ac26a5cb99d48c1f1
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-13 11:58:47 +0300

    Switching app state for authenticated users was implemented

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import Header from './components/Header';
 import Auth from './components/Auth';
 import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
+import { useSelector } from 'react-redux';
 
 
 function App() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <>
       <Header />
-      <Auth />
-      <UserProfile />
+      {isAuthenticated ? <UserProfile /> : <Auth />}
       <Counter />
     </>
   );
