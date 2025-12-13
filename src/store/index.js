f8d24c179cf4ac57a561b09951a12fbafb600809
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-13 13:15:49 +0300

    State slices and actions related to them were moved to separate files

--- src/store/index.js
+++ src/store/index.js
@@ -1,55 +1,12 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const counterInitState = {
-  counter: 0,
-  showCounter: true,
-};
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: counterInitState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    toggle(state) {
-      state.showCounter = !state.showCounter;
-    },
-  }
-});
-
-const authInitState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: 'authentication',
-  initialState: authInitState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  }
-});
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from './auth';
+import counterReducer from './counter';
 
 const store = configureStore({
   reducer: {
-    auth: authSlice.reducer,
-    counter: counterSlice.reducer,
+    auth: authReducer,
+    counter: counterReducer,
   },
 });
-
-export const counterActions = counterSlice.actions;
-
-export const authActions = authSlice.actions;
 
 export default store;
